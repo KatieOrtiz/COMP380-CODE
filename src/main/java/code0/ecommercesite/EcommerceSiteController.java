55b9b9b1f633b11f5f46815297adb539298ea100
Katie Ortiz <62156380+KatieOrtiz@users.noreply.github.com>
2022-11-04 02:47:20 -0700

    Added pages and backend code for signup form

--- src/main/java/code0/ecommercesite/EcommerceSiteController.java
+++ src/main/java/code0/ecommercesite/EcommerceSiteController.java
@@ -1,16 +1,70 @@
 package code0.ecommercesite;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+import java.util.Arrays;
+import java.util.List;
+import javax.validation.Valid;
+import org.springframework.validation.BindingResult;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-
-@RestController
+@Controller
 public class EcommerceSiteController {
-    @RequestMapping("/home")
-    public ModelAndView welcome() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("home.html");
-        return modelAndView;
-    }  
+	
+	@GetMapping("/signup")
+	public String showForm(Model model) {
+		User user = new User();
+		model.addAttribute("user", user);
+		
+		List<String> listProfession = Arrays.asList("Developer", "Tester", "Architect");
+		model.addAttribute("listProfession", listProfession);
+		
+		return "signup_form";
+	}
+
+	@PostMapping("/signup")
+	public String submitForm(@Valid @ModelAttribute("user") User user, BindingResult bindingResult, Model model) {
+		System.out.println(user);
+		
+		if (bindingResult.hasErrors()) {
+			List<String> listProfession = Arrays.asList("Developer", "Tester", "Architect");
+			model.addAttribute("listProfession", listProfession);
+			
+			return "signup_form";
+		}else {
+			return "signup_success";
+		}
+	}
+
+	@GetMapping("/home")
+	public String home() {		
+		return "home";
+	}
+
+	@GetMapping("/cart")
+	public String cart() {		
+		return "cart";
+	}
+
+	@GetMapping("/login")
+	public String login() {		
+		return "login";
+	}
+	
+	@GetMapping("/about")
+	public String about() {		
+		return "about";
+	}
+
+	@GetMapping("/faq")
+	public String faq() {		
+		return "faq";
+	}
+
+	@GetMapping("/contact")
+	public String contact() {		
+		return "contact";
+	}
 }
