6556c4f305d5d0fb2d416d924353a5b8ad59f94a
RmataM7 <rmata.muadthong.250@my.csun.edu>
2022-12-01 22:54:48 -0800

    Corrected minor indentation

--- src/main/java/code0/ecommercesite/EcommerceSiteController.java
+++ src/main/java/code0/ecommercesite/EcommerceSiteController.java
@@ -1,6 +1,5 @@
 package code0.ecommercesite;
 
-
 import java.util.Arrays;
 import java.util.List;
 import javax.validation.Valid;
@@ -13,10 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EcommerceSiteController {
-	
-	
-	/** 
+
+	/**
 	 * Mapping for the /signup page and displays the form for user signup.
+	 * 
 	 * @param model holds application data, which is displayed in the view
 	 * @return String return the html page signup_form
 	 */
@@ -24,78 +23,83 @@ public class EcommerceSiteController {
 	public String showForm(Model model) {
 		User user = new User();
 		model.addAttribute("user", user);
-		
+
 		List<String> listProfession = Arrays.asList("Developer", "Tester", "Architect");
 		model.addAttribute("listProfession", listProfession);
-		
+
 		return "signup_form";
 	}
 
-	
-	/** 
-	 * Mapping for the /signup page after user clicks the button to signup and checks user signup form for errors.
-	 * @param user the user that signed up 
-	 * @param bindingResult Spring's object that holds the result of the validation of the model object, the binding, and any errors that may have occurred  
-	 * @param model holds application data, which is displayed in the view
-	 * @return String return the html page signup_form if errors or return html page signup_success if successful
+	/**
+	 * Mapping for the /signup page after user clicks the button to signup and
+	 * checks user signup form for errors.
+	 * 
+	 * @param user          the user that signed up
+	 * @param bindingResult Spring's object that holds the result of the validation
+	 *                      of the model object, the binding, and any errors that
+	 *                      may have occurred
+	 * @param model         holds application data, which is displayed in the view
+	 * @return String return the html page signup_form if errors or return html page
+	 *         signup_success if successful
 	 */
 	@PostMapping("/signup")
 	public String submitForm(@Valid @ModelAttribute("user") User user, BindingResult bindingResult, Model model) {
 		System.out.println(user);
-		
+
 		if (bindingResult.hasErrors()) {
 			List<String> listProfession = Arrays.asList("Developer", "Tester", "Architect");
 			model.addAttribute("listProfession", listProfession);
-			
+
 			return "signup_form";
-		}else {
+		} else {
 			return "signup_success";
 		}
 	}
 
-	
-	/** 
+	/**
 	 * Mapping for the /home page.
+	 * 
 	 * @return String return the html page home
 	 */
 	@GetMapping("/home")
-	public String home() {		
+	public String home() {
 		return "home";
 	}
 
-	
-	/** 
+	/**
 	 * Mapping for the /checkout page.
+	 * 
 	 * @return String return the html page checkout
 	 */
 	@GetMapping("/checkout")
-	public String checkout() {		
+	public String checkout() {
 		return "checkout";
 	}
 
-	
-	/** 
+	/**
 	 * Mapping for the /products page.
+	 * 
 	 * @return String return the html page products
 	 */
 	@GetMapping("/products")
-	public String products(){
+	public String products() {
 		return "products";
 	}
 
-	
-	/** 
+	/**
 	 * Mapping for the /cart page.
+	 * 
 	 * @return String return the html page cart
 	 */
 	@GetMapping("/cart")
-	public String cart() {		
+	public String cart() {
 		return "cart";
 	}
 
-	
-	/** 
-	 * Mapping for /login, connects to login page and displays the login form for user login.
+	/**
+	 * Mapping for /login, connects to login page and displays the login form for
+	 * user login.
+	 * 
 	 * @param model holds application data, which is displayed in the view
 	 * @return String return the html page login
 	 */
@@ -103,23 +107,27 @@ public class EcommerceSiteController {
 	public String showForm1(Model model) {
 		User user = new User();
 		model.addAttribute("user", user);
-		
+
 		List<String> listProfession = Arrays.asList("Developer", "Tester", "Architect");
 		model.addAttribute("listProfession", listProfession);
-		
+
 		return "login";
 	}
 
-	
-	/** 
-	 * Mapping for the /login page after user clicks the log in button and checks user login form for errors.
-	 * @param user1 the user that logs in 
-	 * @param bindingResult Spring's object that holds the result of the validation of the model object, the binding, and any errors that may have occurred
-	 * @param model holds application data, which is displayed in the view
+	/**
+	 * Mapping for the /login page after user clicks the log in button and checks
+	 * user login form for errors.
+	 * 
+	 * @param user1         the user that logs in
+	 * @param bindingResult Spring's object that holds the result of the validation
+	 *                      of the model object, the binding, and any errors that
+	 *                      may have occurred
+	 * @param model         holds application data, which is displayed in the view
 	 * @return String return the html page login
 	 */
-	//go from login to loginsuccess, reads user.txt, if username and pw are same from what is entered to what is in user.txt, 
-	//it will allow to go to login success, otherwise stay in login
+	// go from login to loginsuccess, reads user.txt, if username and pw are same
+	// from what is entered to what is in user.txt,
+	// it will allow to go to login success, otherwise stay in login
 	@PostMapping("/login")
 	public String submitForm1(@Valid @ModelAttribute("user") User user1, BindingResult bindingResult, Model model) {
 		readfile reading = new readfile();
@@ -129,52 +137,50 @@ public class EcommerceSiteController {
 		String g = user1.getPassword();
 		String h = reading.getB();
 		String i = reading.getD();
-		if(f.equals(h)){
-			if(g.equals(i)){
+		if (f.equals(h)) {
+			if (g.equals(i)) {
 				user1.setName(reading.getF());
 				return "login_success";
-			}else{
+			} else {
 				return "login";
 			}
-		}
-		else{
+		} else {
 			return "login";
 		}
 	}
 
-	
-	
-	/** 
+	/**
 	 * Mapping for the /about page.
+	 * 
 	 * @return String return the html page about
 	 */
 	@GetMapping("/about")
-	public String about() {		
+	public String about() {
 		return "about";
 	}
 
-	
-	/** 
+	/**
 	 * Mapping for the /faq page.
+	 * 
 	 * @return String return the html page faq
 	 */
 	@GetMapping("/faq")
-	public String faq() {		
+	public String faq() {
 		return "faq";
 	}
 
-	
-	/** 
+	/**
 	 * Mapping for the /contact page.
+	 * 
 	 * @return String return the html page contact
 	 */
 	@GetMapping("/contact")
-	public String contact() {		
+	public String contact() {
 		return "contact";
 	}
 
 	@GetMapping("/payment")
-	public String payment() {		
+	public String payment() {
 		return "payment";
 	}
 }
