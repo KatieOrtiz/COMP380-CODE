36c24382fdf3b2d6a0191ecd8c582ec758fa32e6
RmataM7 <rmata.muadthong.250@my.csun.edu>
2022-12-02 16:27:40 -0800

    Unit testing: basic setup

---
+++ src/test/java/code0/ecommercesite/ControllerTest.java
@@ -0,0 +1,13 @@
+package code0.ecommercesite;
+
+//import junit 4 testing framework
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.Date;
+import java.lang.reflect.Field;
+import java.io.FileWriter; // Import the FileWriter class
+import java.io.IOException; // Import the IOException class to handle errors
+
+public class ControllerTest {
+    // @Test
+}
--- src/test/java/code0/ecommercesite/EcommerceSiteApplicationTests.java
+++ src/test/java/code0/ecommercesite/EcommerceSiteApplicationTests.java
@@ -1,7 +1,14 @@
 package code0.ecommercesite;
 
+//import junit 4 testing framework
+import org.junit.Test;
+//import org.junit.Rule;
+//import static org.junit.Assert.*;
+//import org.junit.contrib.java.lang.system.SystemOutRule; 
+//import org.junit.contrib.java.lang.system.TestFromStandardInputStream;
+//import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.*;
 
+//import java.util.ArrayList;
 class EcommerceSiteApplicationTests {
 
-	
 }
